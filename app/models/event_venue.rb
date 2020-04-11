a7e0d1e0480a2990f3c77e43288b2e8d41ac40f8
matias19970 <mireyes@miuandes.cl>
2020-04-11 13:18:52 -0400

    commit 1

--- app/models/event_venue.rb
+++ app/models/event_venue.rb
@@ -1,3 +1,7 @@
 class EventVenue < ApplicationRecord
   has_many :events
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :capacity, numericality: { greater_than: 9}
+
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,11 +3,12 @@ class Ticket < ApplicationRecord
   belongs_to :ticket_type
 
   # TODO: complete the folowing
-  # before_xxxxx :update_stats
+  before_validation :update_stats, on: [ :create, :destroy ]
 
   private
     def update_stats
       es = self.ticket_type.event.event_stat
+      es.attendance += 1
       # TODO: complete in order to update event stats
     end
 end
